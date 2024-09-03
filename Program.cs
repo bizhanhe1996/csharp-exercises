d212ff4b85a88a19421af78d7c3508e1ddf00280
sepehr <bizhan.he1996@gmail.com>
2024-09-03 21:27:19 +0330

    Le Premier

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace csharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string firstName = "Bizhan";
+        string lastName = "Hejazi";
+        string fullName = firstName + " " + lastName;
+
+        Console.WriteLine(fullName);
+    }
+}
